bbe0a5cce109595ee5a1ccd6991ccb5c3954a7a6
Kelly Harris <zanyfrog@gmail.com>
2019-11-19 14:29:02 -0500

    ion-grid completed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {EventResponse} from '../interfaces';
+import { Subscription } from 'rxjs';
+import { EventsService } from '../events.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription
 
-  constructor() {}
+  constructor(private eventService: EventsService,
+    private nav: NavController) 
+    {
+      console.log('Home Page');
+    }
+
+  ngOnInit(): void
+  {
+    this.sub = this.eventService.getAll()
+      .subscribe( e => this.events.push(e));
+  }
+
+  ngOnDestroy(): void
+  {
+    this.sub.unsubscribe();
+  }
+
+  getEvents(): EventResponse[]
+  {
+    return this.events.sort( (a,b) => a.event.created > b.event.created ? -1 : 1);
+  }
+
+  details(response: EventResponse)
+  {
+    this.nav.navigateForward( '/details/${response.event.id}');
+  }
 
 }
